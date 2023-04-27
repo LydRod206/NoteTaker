57c47037f5ed0ec4744008e544ce437aa6f45179
LydRod206 <119384486+LydRod206@users.noreply.github.com>
2023-04-27 16:02:47 -0700

    server and routes

---
+++ server.js
@@ -0,0 +1,46 @@
+// require dependencies
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const nanoid = require('nanoid');
+// create express app
+const app = express();
+const PORT = process.env.PORT || 3001;
+// middleware
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+// ROUTES
+// GET route for homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+// GET route for notes page
+app.get('/api/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+// GET route for getting all notes
+app.get('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    res.json(notes);
+});
+// POST route for adding a new note
+app.post('/api//api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = nanoid();
+    const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    notes.push(newNote);
+    fs.writeFileSync('./db/db.json'. JSON.stringify(notes));
+    res.json(newNote);
+});
+// DELETE route for deleting a note by ID
+app.delete('/api/notes/:id', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    const updatedNotes = notes.filter(note => note.id !== req.params.id);
+    fs.writeFileSync('./db/db.json', JSON.stringify(updatedNotes));
+    res.json({success: true});
+});
+// LISTEN for server
+app.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}`);
+});
